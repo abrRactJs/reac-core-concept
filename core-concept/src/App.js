a76e2bbccf1e198f57aa88e6def4d071a9d3b8e5
abrRactJs <mdb881700@gmail.com>
2022-12-12 13:06:22 +0600

    used rest api

--- core-concept/src/App.js
+++ core-concept/src/App.js
@@ -1,95 +1,48 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-
-  const containerStyle = {
-    width: "85%",
-    margin: "50px auto",
-  };
-
-
+function App(){
+  const AppStyle = {
+      width: "70%",
+      margin : "auto"
+  }
+ 
+  const [user, setUser] = useState([])
+  useEffect(()=>{
+     fetch('https://jsonplaceholder.typicode.com/comments')
+     .then(res => res.json())
+     .then(data => setUser(data))
+  }, [])
 
   return (
-    <div style={containerStyle}>
-      <Hello></Hello>
-      <Counter></Counter>
-      <Users></Users>
-      <Todos></Todos>
-    </div>
-  );
-}
-
-
-function Counter(){
-  const [count, setCount] = useState(0);
-  const decressHandle = ()=> setCount(count - 1) ;
-  return(
-     <div>
-       <h1>Count: {count}</h1>
-       <button onClick={decressHandle}>Decress</button>
-       <button onClick={ () => setCount(count + 1) }>Incress</button>
+     <div style={AppStyle}>
+       <h1>hello react js </h1>
+       <ParsonInfo></ParsonInfo>
+       {
+        console.log(user)
+       }
+        {
+          user.map(usrInfo => <ParsonInfo name={usrInfo.name} email={usrInfo.email} post={usrInfo.body}></ParsonInfo> )
+        }
      </div>
   )
 }
 
-
-function Hello() {
-  return (
-    <div>
-      <h2>Hello react js</h2>
-    </div>
-  )
+function ParsonInfo(props){
+  const parsonStyle ={
+     wodth : "100%",
+     padding : "20px 10px",
+     border: "2px solid black",
+     margin : "20px 0px"
+  }
+   return (
+     <div style={parsonStyle}>
+       <h2>User Name :  <span style={{color:"red"}}>{props.name} </span></h2>
+       <h2>User Email: <span style={{color:"green"}}>{props.email}</span> </h2>
+       <p style={{textAlign :"center"}}>{props.post}</p>
+     </div>
+   )
 }
-
-
-function Users(){
-  const [user, setUsers] = useState([]);
-  useEffect( ()=>{
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(data => setUsers(data))
-    
-  } )
-  return(
-    <div>
-       <h2> Dynamic User Infirmation : {user.length} </h2>
-       <ul>
-        {
-          user.map(userName => <li>{userName.name}</li>)
-        }
-        {
-          
-          user.map(userPhone => <li>{userPhone.phone}</li>)
-        }
-       </ul>
-    </div>
-  )
-}
-
-
-function Todos(){
-  const [user , setUser] = useState([])
-  useEffect( ()=>{
-     fetch("https://jsonplaceholder.typicode.com/todos")
-     .then(res => res.json())
-     .then(data => setUser(data))
-  },[] )
-  return (
-    <div>
-       <h2>Todos list here </h2>
-       {
-        console.log(user)
-       }
-       <ul>
-        {
-          user.map(userName => <li>{userName.title}</li> )
-        }
-       </ul>
-    </div>
-  )
-}
-
 
 
 
